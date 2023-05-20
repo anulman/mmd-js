d13ce5e22c7ad88a6b232668be409f9e7a4bbb17
@anulman <anulman@gmail.com>
2023-05-20 18:40:58 -0400

    add shields to nextjs demo

--- demo/src/app/layout.tsx
+++ demo/src/app/layout.tsx
@@ -10,7 +10,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <footer style={{ display: "flex", gap: "0.5rem", marginTop: "-1rem" }}>
+          <a href="https://npmjs.com/package/mmd-js">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://img.shields.io/npm/v/mmd-js?label=npm&logo=npm"
+              alt="github version"
+            />
+          </a>
+          <a href="https://github.com/anulman/mmd-js">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://img.shields.io/github/package-json/v/anulman/mmd-js/main?label=github&logo=github"
+              alt="github version"
+            />
+          </a>
+          <a href="https://twitter.com/intent/follow?screen_name=anulman">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://img.shields.io/twitter/follow/anulman?style=social&logo=twitter"
+              alt="follow on Twitter"
+            />
+          </a>
+        </footer>
+      </body>
     </html>
   );
 }
